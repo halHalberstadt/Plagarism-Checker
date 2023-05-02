bdf33049d2f29b369910ac285121343a91ec9304
halHalberstadt <halberstadtsterling@gmail.com>
2023-05-01 18:06:09 -0700

    displays to user now

--- myExpressApp/public/javascripts/script.js
+++ myExpressApp/public/javascripts/script.js
@@ -4,87 +4,66 @@ var buttonQ = document.querySelector("#submitQ");
 var buttonF = document.querySelector("#submitF");
 var resultsText = document.querySelector("#results");
 
-console.log(buttonF);
 //Adds listeners for buttons in web scraper page, if button is clicked then the corresponding functions execute
 buttonQ.addEventListener("click", searchQuestion);
 buttonF.addEventListener("click", searchFile);
-console.log("script here");
 
-async function searchTest(endpoint, document){
-  // console.log(`http://website-spring.herokuapp.com/` + endpoint + "?ordered=False");
-  // console.log(document);
-  // let docText = JSON.stringify(document);
-  // document = document + "?";
-  let res = await fetch(`http://website-spring.herokuapp.com/` + endpoint, {
-    method: "POST",
-    body: document
-  })
-  // }).then((res) => res.json())
-  console.log("response", res);
-  // let obj = await response.json();
-  let obj = await res.text();
-  console.log(obj.value);
-  return obj;
-};
-
-// function for single question search
-async function searchQuestion(){
-  console.log("value = ", document.querySelector("#searchQ").value);
-  let obj = await searchTest("text?ordered=False", document.querySelector("#searchQ").value );
-  console.log("Search Test", obj);
-  // obj.queries.forEach(element => {
-  //   resultsText.innerHTML += element.queryText;
-  // });
-
- 
-}
-
-//Testing function
-// async function uploadFile() {
-//   let formData = new FormData(); 
-//   formData.append("fileupload", fileupload.files[0]);
-//     await fetch(`http://website-spring.herokuapp.com/` + endpoint, {
-//     method: "POST", 
-//     body: formData
-//   }); 
+// async function searchTest(endpoint, document) {
+//   let res = await fetch(`http://website-spring.herokuapp.com/` + endpoint, {
+//     method: "POST",
+//     body: document,
+//   }).then((res) => res.json())
+//   console.log("response", res);
+//   // let obj = await response.json();
+//   let obj = await res.text();
+//   return obj;
 // }
 
-// function for file search
-async function searchFile(){
-  var input = document.querySelector('input[type="file"]')
-  var data = new FormData();
-  data.append('document', input[0]);
-  // data = input[0]
-  // console.log(data.values());
-  // console.log("data", input.value);
-  let obj = await searchTest("word?search=False", data);
-  console.log("Search Test ->", obj);
+// function for single question search
+async function searchQuestion() {
+  // console.log("value = ", document.querySelector("#searchQ").value);
 
-  
+  let res = await fetch(
+    `http://website-spring.herokuapp.com/text?ordered=False`,
+    {
+      method: "POST",
+      body: document.querySelector("#searchQ").value + "?",
+    }
+  ).then((res) => res.json());
+
+  let obj = await res;
+
+  document.querySelector("#results").innerHTML = obj;
 }
 
-// function HtmlFormRequest( url, input, cb ) 
-// { 
-//     var i, args = ""; 
-    
-//     if( input.enctype == 'multipart/form-data' ) { 
-//     	args = new FormData(input); 
-//     } else { 
-// 	    for( i in input.elements ) { 
-// 	        if( input.elements[i].type != 'button' ) { 
-// 	            if( args != "" ) args += "&"; 
-// 	            args += encodeURIComponent(input.elements[i].name) + "=" + encodeURIComponent(input.elements[i].value); 
-// 	        } 
-// 	    } 
-//     } 
-//     HtmlRequest(url, args, cb); 
-// } 
+async function uploadFile(object) {
+  try {
+    res = await fetch(`http://website-spring.herokuapp.com/word?search=False`, {
+      method: "POST",
+      body: object
+    });
+  } catch (err) {
+    console.log(err);
+  } finally {
+    return await res.json();
+  }
+}
 
-// async function searchTest(){
-//   let response = await fetch(`http://website-spring.herokuapp.com/exampleQuery`, {
-//     method: "GET"
-//   })
-//   let obj = await response.json();
-//   console.log(obj);
-//   return obj;
-// };
+// function for file search
+async function searchFile() {
+  let input = document.querySelector('input[type="file"]');
+
+  let data = new FormData();
+  data.append("document", input.files[0]);
+
+  let res = await uploadFile(data);
+
+  // console.log("res", res.queries);
+  let printableQueries = res.queries;
+  console.log(printableQueries[0]);
+  for(var i = 0; i < printableQueries.length; i++){
+    var text = printableQueries[i].queryText
+    console.log(text);
+    document.querySelector("#results").innerHTML += text + "\n";
+  }
+}
